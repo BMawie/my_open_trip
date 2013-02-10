981df1e8cb5e162ba342f6ea7bfd7f8b4a91f665
Marie Battestini <mbattest@gmail.com>
2013-02-10 17:42:51 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyOpenTrip::Application.config.secret_token = '6b42b9e44b8c7665b82c840630aea7209a6e6f954474bd96103935bcec732289c3d0fa42d08459a24a66253c8384ed1ccd7d712fefdc6cae3b496b56a4d38a4d'
